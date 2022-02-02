18e77f921b9134de1df78ee40c34b17f5e079f46
Matheus Najjar <matheusnajjar@gmail.com>
2022-02-02 18:51:24 -0300

    resolve problema 11

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/11.js
@@ -0,0 +1,17 @@
+const porcentagem = 65;
+
+if (porcentagem >= 90) {
+    console.log('A');
+} else if (porcentagem >= 80) {
+    console.log('B');
+} else if (porcentagem >= 70) {
+    console.log('C');
+} else if (porcentagem >= 60) {
+    console.log('D');
+} else if (porcentagem >= 50) {
+    console.log('E');
+} else if (porcentagem < 50) {
+    console.log('F');
+} else {
+    console.log('Erro!')
+}
