471fc18ecc8d196ee5a1f5f5cc6fad514f6c41b1
Matheus Najjar <matheusnajjar@gmail.com>
2022-02-02 19:51:50 -0300

    renomeia arquivos 12.js e 13.js

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/12.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/12.js
@@ -1,8 +1,8 @@
 const num1 = 1;
 const num2 = 2;
-const num3 = 4;
+const num3 = 3;
 
-if (num1 % 2 == 1 || num2 % 2 == 1 || num3 % 2 == 1) {
+if (num1 % 2 == 0 || num2 % 2 == 0 || num3 % 2 == 0) {
     console.log(true)
 } else {
     console.log(false)
